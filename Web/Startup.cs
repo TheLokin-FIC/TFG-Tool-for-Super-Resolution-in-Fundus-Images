67354fdb86a88055fd5bc69acf2691f2065df240
TheLokin <diego.ramil.lopez@udc.es>
2021-05-16 17:29:53 +0200

    Added loading indicator

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -42,7 +42,6 @@ namespace Web
             }
 
             app.UseStaticFiles();
-
             app.UseMvc();
         }
     }
--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -1,17 +1,18 @@
-(function () {
-    'use strict';
-    $('.input-file').each(function () {
-        var $input = $(this);
-        var $label = $input.next('.btn-file');
-        var labelValue = $label.html();
+$('.input-file').each(function () {
+    var $input = $(this);
+    var $label = $input.next('.btn-file');
+    var labelValue = $label.html();
 
-        $input.on('change', function (element) {
-            if (element.target.value) {
-                var filename = element.target.value.split('\\').pop();
-                $label.addClass('has-file').find('.js-filename').html(filename);
-            } else {
-                $label.removeClass('has-file').html(labelValue);
-            }
-        });
+    $input.on('change', function (element) {
+        if (element.target.value) {
+            var filename = element.target.value.split('\\').pop();
+            $label.addClass('has-file').find('.js-filename').html(filename);
+        } else {
+            $label.removeClass('has-file').html(labelValue);
+        }
     });
-})();
+});
+
+$('form').submit(function () {
+    $('.loading').css('visibility', 'visible');
+});
